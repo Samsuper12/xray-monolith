8d9f0b02c2d97e9e55df19c10268d3dd93d09b51
ProfLander <1253239+ProfLander@users.noreply.github.com.>
2025-09-07 04:03:53 +0100

    Add release flags for Clang

--- CMake/XRay.Compiler.Clang.cmake
+++ CMake/XRay.Compiler.Clang.cmake
@@ -16,6 +16,14 @@ set(XRAY_COMPILER_FLAGS
   -Wuninitialized
 )
 
+set(XRAY_COMPILER_FLAGS_RELEASE
+  # Not Debug
+  -DNDEBUG
+  # Disable iterator debugging
+  -D_HAS_ITERATOR_DEBUGGING=0
+  -D_SECURE_SCL=0
+)
+
 # Explicitly state when we're compiling for Win32
 if(WIN32)
   list(APPEND XRAY_COMPILER_FLAGS -DWIN32)
