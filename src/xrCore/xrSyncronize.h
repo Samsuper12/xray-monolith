e0b627d99902eb1e777fcba59334ed8b231da8e7
Michael Skorokhodov <teethfall@icloud.com>
2026-03-19 11:42:24 +0200

    rewrite xrSyncronize

--- src/xrCore/xrSyncronize.h
+++ src/xrCore/xrSyncronize.h
@@ -3,21 +3,6 @@
 #include <mutex>
 #include <shared_mutex>
 #include "_noncopyable.h"
-#if 0//def DEBUG
-# define PROFILE_CRITICAL_SECTIONS
-#endif // DEBUG
-
-#ifdef PROFILE_CRITICAL_SECTIONS
-typedef void(*add_profile_portion_callback) (LPCSTR id, const u64& time);
-void XRCORE_API set_add_profile_portion(add_profile_portion_callback callback);
-
-# define STRINGIZER_HELPER(a) #a
-# define STRINGIZER(a) STRINGIZER_HELPER(a)
-# define CONCATENIZE_HELPER(a,b) a##b
-# define CONCATENIZE(a,b) CONCATENIZE_HELPER(a,b)
-# define MUTEX_PROFILE_PREFIX_ID #mutexes/
-# define MUTEX_PROFILE_ID(a) STRINGIZER(CONCATENIZE(MUTEX_PROFILE_PREFIX_ID,a))
-#endif // PROFILE_CRITICAL_SECTIONS
 
 // Desc: Simple wrapper for critical section
 class XRCORE_API xrCriticalSection : xray::noncopyable
@@ -34,24 +19,16 @@ public:
 	};
 
 private:
-	void* pmutex;
-#ifdef PROFILE_CRITICAL_SECTIONS
-    LPCSTR m_id;
-#endif // PROFILE_CRITICAL_SECTIONS
-
+	std::recursive_mutex mutex;
 public:
-#ifdef PROFILE_CRITICAL_SECTIONS
-    xrCriticalSection(LPCSTR id);
-#else // PROFILE_CRITICAL_SECTIONS
 	xrCriticalSection();
-#endif // PROFILE_CRITICAL_SECTIONS
 	~xrCriticalSection();
 
 	void Enter();
 	void Leave();
 	BOOL TryEnter();
 
-	bool IsValid() { return pmutex != nullptr; }
+	bool IsValid() { return true; }
 };
 
 class xrCriticalSectionGuard : xray::noncopyable
