aa1d8e3b8edd57488ac3ebe27f34f5e42e3aa593
Michael Skorokhodov <teethfall@icloud.com>
2026-07-28 22:21:33 +0300

    log: update codebase

--- src/xrCore/log.h
+++ src/xrCore/log.h
@@ -1,34 +1,36 @@
 #ifndef logH
 #define logH
 
-#ifdef WIN32
-//#include <windows.h>
-#endif
 #include "xrstring.h"
 #include "_vector3d.h"
+#include <cstdio>
 
 #define VPUSH(a) ((a).x), ((a).y), ((a).z)
 
-void XRCORE_API __cdecl Msg(LPCSTR format, ...);
-void XRCORE_API Log(LPCSTR msg);
-void XRCORE_API Log(LPCSTR msg);
-void XRCORE_API Log(LPCSTR msg, LPCSTR dop);
-void XRCORE_API Log(LPCSTR msg, u32 dop);
-void XRCORE_API Log(LPCSTR msg, int dop);
-void XRCORE_API Log(LPCSTR msg, float dop);
-void XRCORE_API Log(LPCSTR msg, const Fvector& dop);
-void XRCORE_API Log(LPCSTR msg, const Fmatrix& dop);
-void XRCORE_API LogWinErr(LPCSTR msg, long err_code);
+void Msg(LPCSTR format, ...);
+void Log(LPCSTR msg);
+void Log(LPCSTR msg);
+void Log(LPCSTR msg, LPCSTR dop);
+void Log(LPCSTR msg, u32 dop);
+void Log(LPCSTR msg, int dop);
+void Log(LPCSTR msg, float dop);
+void Log(LPCSTR msg, const Fvector& dop);
+void Log(LPCSTR msg, const Fmatrix& dop);
+void LogWinErr(LPCSTR msg, long err_code);
+inline auto NeedAttention(const char* hint = nullptr, const char* filename = __FILE__, const char* func = __func__) -> void {
+    Msg("Attention required here. Debug it! [%s:%s]. Hint: %s\n", filename, func, hint ? hint : "None");
+    fprintf(stderr, "Attention required here. Debug it! [%s:%s]. Hint: %s\n", filename, func, hint ? hint : "None");
+}
 
 typedef void (*LogCallback)(LPCSTR string);
-LogCallback XRCORE_API SetLogCB(LogCallback cb);
-void XRCORE_API CreateLog(BOOL no_log = FALSE);
+LogCallback SetLogCB(LogCallback cb);
+void CreateLog(BOOL no_log = FALSE);
 void InitLog();
 void CloseLog();
-void XRCORE_API FlushLog();
+void FlushLog();
 
-extern XRCORE_API xr_vector<shared_str>* LogFile;
-extern XRCORE_API BOOL LogExecCB;
+extern xr_vector<shared_str>* LogFile;
+extern BOOL LogExecCB;
 
 shared_str FormatString(LPCSTR fmt, ...);
 
